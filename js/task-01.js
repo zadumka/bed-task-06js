fa15b333c18accfe2f32a17171501f2dd9b00716
zadumka <90329284+zadumka@users.noreply.github.com>
2023-07-05 12:55:36 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -2,6 +2,6 @@ const categoriesList = document.querySelector('#categories');
 console.log(`Number of categories:`, categoriesList.children.length);
 
 [...categoriesList.children].forEach(elem => {
-    console.log(`Category: ${elem.firstElementChild.textContent}`);
-    console.log(`Elements:`, elem.lastElementChild.children.length);
+    console.log(`Category: ${elem.firstElementChildtextContent}`);
+    console.log(`Elements:`, elem.lastElementChild.childrenlength);
 });
